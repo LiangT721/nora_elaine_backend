366bddae73cad32c6d71e002bc405b085be1a9c6
TangLian <96833954+LiangT721@users.noreply.github.com>
2022-10-06 10:28:39 -0600

    change sort

--- middleware/get_date.js
+++ middleware/get_date.js
@@ -16,8 +16,10 @@ const current_date = () => {
 
 const get_time = () => {
   let time = new Date();
+  console.log(time)
   return time
 }
+
 
 exports.current_date = current_date;
 exports.get_time = get_time;
